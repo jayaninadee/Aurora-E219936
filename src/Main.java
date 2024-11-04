c1836c5e6e8e7be3d2032043a90664ff393825eb
jayan <jayaninadeeshaa@gmail.com>
2024-11-04 18:29:56 +0530

    DB Creation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//    DB Connection
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/auroraskincare";
+    private static final String USER = "jayan";
+    private static final String PASS = " ";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int choice;
+        // Do - While
+        do{
+            System.out.println("\n________________________");
+            System.out.println("Aurora Skin Care Clinic");
+            System.out.println("------------------------");
+            System.out.println("1. Make Appointment");
+            System.out.println("2. Update Appointment");
+            System.out.println("3. View Appointments");
+            System.out.println("4. Search Appointments");
+            System.out.println("5. Accept Registration Fee");
+            System.out.println("6. Calculate and Generate Invoice");
+            System.out.println("7. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = s.nextInt();
+            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    // Code to make an appointment
+                    System.out.println("Making an appointment...");
+                    break;
+                case 2:
+                    // Code to update an appointment
+                    System.out.println("Updating an appointment...");
+                    break;
+                // ... other cases ...
+                case 7:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }while (choice !=7);
+        s.close();
     }
 }
