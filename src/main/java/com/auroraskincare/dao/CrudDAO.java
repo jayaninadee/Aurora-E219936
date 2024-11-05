ebf6248c1816ad0bba60387d4fe451687263dfe2
jayan <jayaninadeeshaa@gmail.com>
2024-11-05 18:56:43 +0530

    DAO initialized

--- src/main/java/com/auroraskincare/dao/CrudDAO.java
+++ src/main/java/com/auroraskincare/dao/CrudDAO.java
@@ -7,5 +7,21 @@
 
 package com.auroraskincare.dao;
 
-public interface CrudDAO {
+import java.util.ArrayList;
+
+public interface CrudDAO <T,ID> extends SuperDAO{
+
+    public ArrayList<T> getAll()throws Exception;
+
+    public T get(ID id)throws Exception;
+
+    public boolean add(T entity)throws Exception;
+
+    public boolean update(T entity)throws Exception;
+
+    public boolean delete(ID id)throws Exception;
+
+    public ArrayList<T> search(ID id)throws Exception;
+
+    public String getId() throws Exception;
 }
