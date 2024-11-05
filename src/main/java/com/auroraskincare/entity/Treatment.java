4119ff6fa3c9dd2acb61807480b3206825180767
jayan <jayaninadeeshaa@gmail.com>
2024-11-05 18:22:26 +0530

    Treatment entity

--- src/main/java/com/auroraskincare/entity/Treatment.java
+++ src/main/java/com/auroraskincare/entity/Treatment.java
@@ -10,6 +10,39 @@ package com.auroraskincare.entity;
 public class Treatment {
     private int trID;
     private String trName;
-    private String trName;
-    private String trName;
+    private double trprice;
+
+    public Treatment() {
+    }
+
+    public Treatment(int trID, String trName, double trprice) {
+        this.trID = trID;
+        this.trName = trName;
+        this.trprice = trprice;
+    }
+
+    public int getTrID() {
+        return trID;
+    }
+
+    public void setTrID(int trID) {
+        this.trID = trID;
+    }
+
+    public String getTrName() {
+        return trName;
+    }
+
+    public void setTrName(String trName) {
+        this.trName = trName;
+    }
+
+    public double getTrprice() {
+        return trprice;
+    }
+
+    public void setTrprice(double trprice) {
+        this.trprice = trprice;
+    }
+
 }
