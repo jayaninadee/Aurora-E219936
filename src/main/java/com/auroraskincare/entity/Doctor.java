1bcdbfb37c31968e69ce8571a3565fb426f3a7e6
jayan <jayaninadeeshaa@gmail.com>
2024-11-05 15:30:09 +0530

    Doctor entity

--- src/main/java/com/auroraskincare/entity/Doctor.java
+++ src/main/java/com/auroraskincare/entity/Doctor.java
@@ -8,4 +8,71 @@
 package com.auroraskincare.entity;
 
 public class Doctor {
+    private int doctorID;
+    private String dName;
+    private String dSpecialization;
+    private String dNIC;
+    private int dConNumber;
+
+    public Doctor() {
+    }
+
+    public Doctor(int doctorID, String dName, String dSpecialization, String dNIC, int dConNumber) {
+        this.doctorID = doctorID;
+        this.dName = dName;
+        this.dSpecialization = dSpecialization;
+        this.dNIC = dNIC;
+        this.dConNumber = dConNumber;
+    }
+
+    public int getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public String getdSpecialization() {
+        return dSpecialization;
+    }
+
+    public void setdSpecialization(String dSpecialization) {
+        this.dSpecialization = dSpecialization;
+    }
+
+    public String getdNIC() {
+        return dNIC;
+    }
+
+    public void setdNIC(String dNIC) {
+        this.dNIC = dNIC;
+    }
+
+    public int getdConNumber() {
+        return dConNumber;
+    }
+
+    public void setdConNumber(int dConNumber) {
+        this.dConNumber = dConNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "doctorID=" + doctorID +
+                ", dName='" + dName + '\'' +
+                ", dSpecialization='" + dSpecialization + '\'' +
+                ", dNIC='" + dNIC + '\'' +
+                ", dConNumber=" + dConNumber +
+                '}';
+    }
 }
