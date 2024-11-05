38b8436edba27123cb40660d49e97f7f593b53c3
jayan <jayaninadeeshaa@gmail.com>
2024-11-05 15:39:46 +0530

    Patient entity

--- src/main/java/com/auroraskincare/entity/Patient.java
+++ src/main/java/com/auroraskincare/entity/Patient.java
@@ -8,4 +8,71 @@
 package com.auroraskincare.entity;
 
 public class Patient {
+    private int patientID;
+    private String pName;
+    private String pNIC;
+    private String pEmail;
+    private String pConNumber;
+
+    public Patient() {
+    }
+
+    public Patient(int patientID, String pName, String pNIC, String pEmail, String pConNumber) {
+        this.patientID = patientID;
+        this.pName = pName;
+        this.pNIC = pNIC;
+        this.pEmail = pEmail;
+        this.pConNumber = pConNumber;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpNIC() {
+        return pNIC;
+    }
+
+    public void setpNIC(String pNIC) {
+        this.pNIC = pNIC;
+    }
+
+    public String getpEmail() {
+        return pEmail;
+    }
+
+    public void setpEmail(String pEmail) {
+        this.pEmail = pEmail;
+    }
+
+    public String getpConNumber() {
+        return pConNumber;
+    }
+
+    public void setpConNumber(String pConNumber) {
+        this.pConNumber = pConNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientID=" + patientID +
+                ", pName='" + pName + '\'' +
+                ", pNIC='" + pNIC + '\'' +
+                ", pEmail='" + pEmail + '\'' +
+                ", pConNumber='" + pConNumber + '\'' +
+                '}';
+    }
 }
